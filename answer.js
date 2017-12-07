1af1719ab38fd0669ef60d16b7cc9079503c0888
kerri alison <kerrivarner@gmail.com>
2017-12-07 12:29:59 -0500

    Create answer.js

---
+++ answer.js
@@ -0,0 +1,11 @@
+const members = new Map();
+
+members.set('Evelyn', 75.68);
+members.set('Liam', 20.16);
+members.set('Sophia', 0);
+members.set('Marcus', 10.25);
+
+for (const [key, val] of members) {
+    console.log(members);
+    
+}
